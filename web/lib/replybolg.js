9534337e50a82092201ba6d29f9c0792f4f77640
Sohyu1 <783487703@qq.com>
2019-12-18 22:29:12 +0800

    1

---
+++ web/lib/replybolg.js
@@ -0,0 +1,39 @@
+var txt = document.getElementsByClassName('txt')[0];
+var btn = document.getElementsByTagName('button')[0];
+var pl = document.getElementsByClassName('blog-comment')[0];
+var Olen = document.getElementById('len');
+
+txt.onblur = function() {
+    txt.style.width = '450px';
+    txt.style.heigth = '150px'
+}
+txt.oninput = function() {
+    Olen.innerHTML = txt.value.length;
+}
+btn.onclick = function() {
+    if (txt.value == '') {
+        confirm('你确定不出入内容吗')
+        return false;
+    } else {
+        var ul = document.createElement('ul');
+        pl.appendChild(ul);
+        var li = document.createElement('li');
+        ul.appendChild(li);
+        li.innerHTML = txt.value;
+        txt.value = ''
+        var li = document.createElement('li');
+        ul.appendChild(li);
+        var date = new Date();
+        li.innerHTML = date.toLocaleDateString();
+        var oa = document.createElement('a');
+        li.appendChild(oa);
+        oa.innerHTML = '删除';
+        oa.style.float = 'right';
+        oa.style.href = '#'
+    }
+    oa.onclick = function() {
+        oa.parentNode.parentNode.remove()
+        oa.style.cursor = 'pointer'
+    }
+
+}
